e2226fdb24343e2327fba4fbb3602b7732129da9
rumahchiara2 <rumahchiara2@gmail.com>
2025-09-30 01:02:02 +0700

    edit glassy

--- app/src/main/java/com/nunucore/corearsitek/SplashActivity.java
+++ app/src/main/java/com/nunucore/corearsitek/SplashActivity.java
@@ -1,30 +1,56 @@
 package com.nunucore.corearsitek;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import eightbitlab.com.blurview.BlurView;
+import eightbitlab.com.blurview.RenderScriptBlur;
 
 public class SplashActivity extends AppCompatActivity {
-
-    private ImageView shineArsitek;
-    private ImageView shineStudio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // Setup Blur untuk Core Arsitek
+        BlurView blurArsitek = findViewById(R.id.blurCardArsitek);
+        setupBlur(blurArsitek);
+
+        // Setup Blur untuk Core Studio
+        BlurView blurStudio = findViewById(R.id.blurCardStudio);
+        setupBlur(blurStudio);
+
         // Shine untuk Core Arsitek
-        shineArsitek = findViewById(R.id.shineArsitek);
+        ImageView shineArsitek = findViewById(R.id.shineArsitek);
         Animation animArsitek = AnimationUtils.loadAnimation(this, R.anim.shine_animation);
         shineArsitek.startAnimation(animArsitek);
 
         // Shine untuk Core Studio
-        shineStudio = findViewById(R.id.shineStudio);
+        ImageView shineStudio = findViewById(R.id.shineStudio);
         Animation animStudio = AnimationUtils.loadAnimation(this, R.anim.shine_animation);
         shineStudio.startAnimation(animStudio);
+    }
+
+    private void setupBlur(BlurView blurView) {
+        float radius = 20f;
+
+        View decorView = getWindow().getDecorView();
+        View rootView = decorView.findViewById(android.R.id.content);
+        Drawable windowBackground = decorView.getBackground();
+
+        blurView.setupWith((View) rootView)
+                .setFrameClearDrawable(windowBackground)
+                .setBlurAlgorithm(new RenderScriptBlur(this))
+                .setBlurRadius(radius)
+                .setBlurAutoUpdate(true)
+                .setHasFixedTransformationMatrix(true);
     }
 }
