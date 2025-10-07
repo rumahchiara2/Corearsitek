f0e57aa55d489278696d314581ff364dab9073c7
rumahchiara2 <rumahchiara2@gmail.com>
2025-10-08 00:47:02 +0700

    update new look

--- app/src/main/java/com/nunucore/corearsitek/LanguageSelectActivity.java
+++ app/src/main/java/com/nunucore/corearsitek/LanguageSelectActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
-import android.widget.CardView;
+import androidx.cardview.widget.CardView;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
